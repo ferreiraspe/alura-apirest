ffc50e42fe690b5bfbb3f255fa7e5101e107ea76
Edson Ferreira <ferreiraspe@gmail.com>
2022-07-16 18:42:04 -0300

    fix: adding gitignore file

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
 
         [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
@@ -16,6 +17,5 @@ namespace FilmesAPI.Models
 
         [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
-        public int Id { get; set; }
     }
 }
