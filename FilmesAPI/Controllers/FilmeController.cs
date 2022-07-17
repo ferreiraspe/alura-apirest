2c46b4f3d2f9f79ce0a1875bb273fa62d0face6c
Edson Ferreira <ferreiraspe@gmail.com>
2022-07-17 02:23:49 -0300

    feat: implementing the delete method

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -60,5 +60,19 @@ namespace FilmesAPI.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
